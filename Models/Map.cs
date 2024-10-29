26e25e32f25186498e044493bae14a2caaf59ee3
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-10-29 17:55:13 +0100

    Add properties, constructor, and empty methods for Map class

--- Models/Map.cs
+++ Models/Map.cs
@@ -8,5 +8,27 @@ namespace Tank_Tactics.Models
 {
     public class Map
     {
+        public string Name { get; }
+        public Player[] Players { get; }
+        public string Background { get; }
+        public List<MapElement> Elements { get; }
+
+        public Map(string name, Player[] players, string? background, List<MapElement> elements)
+        {
+            Name = name;
+            Players = players;
+            Background = background;
+            Elements = elements;
+        }
+
+        public void MoveTank(double leftChange, double topChange, int tankIndex)
+        {
+           
+        }
+
+        public void Attack(Tank attacker, Tank defender, AttackTypes attackType)
+        {
+
+        }
     }
 }
