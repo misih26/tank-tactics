63ef40c111fd76af6d9a3d9771027f54df51c324
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-10-29 17:45:17 +0100

    Add properties, constructor, and method implementation for MineMapElement class; inherit from MapElement

--- Models/MineMapElement .cs
+++ Models/MineMapElement .cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Tank_Tactics.Models
 {
-    public class MineMapElement
+    public class MineMapElement : MapElement
     {
+        public double Damage { get; }
+
+        public MineMapElement(string image, double positionTop, double positionLeft, double width, double height, double damage)
+            : base(image, positionTop, positionLeft, width, height)
+        {
+            Damage = damage;
+        }
+
+        public override void HandleTouch(Tank tank) { }
     }
 }
