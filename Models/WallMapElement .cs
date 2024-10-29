a6cdd0f89725ebad1424a9e4a6a84e1c7f13b0c7
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-10-29 17:50:17 +0100

    Add constructor and method implementation for WallMapElement class; inherit from MapElement

--- Models/WallMapElement .cs
+++ Models/WallMapElement .cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Tank_Tactics.Models
 {
-    public class WallMapElement
+    public class WallMapElement : MapElement
     {
+        public WallMapElement(string image, double positionTop, double positionLeft, double width, double height)
+            : base(image, positionTop, positionLeft, width, height)
+        { }
+
+        public override void HandleTouch(Tank tank) { }
     }
 }
+
