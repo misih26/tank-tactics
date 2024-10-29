45212cdd4912535e7be629a7b639cdfd961ba138
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-10-29 17:22:31 +0100

    Add properties for Tank class

--- Models/Tank.cs
+++ Models/Tank.cs
@@ -8,5 +8,14 @@ namespace Tank_Tactics.Models
 {
     public class Tank
     {
+        public string Name { get; set; }
+        public double Shield { get; set; }
+        public double Attack { get; set; }
+        public double AttackCooldown { get; set; }
+        public double Speed { get; set; }
+        public double PositionTop { get; set; }
+        public double PositionLeft { get; set; }
+        public Direction Direction { get; set; }
+        public bool Blocked { get; set; }
     }
 }
