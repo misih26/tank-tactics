09ee4d7fb85562e724c3a61c433b56cdafe52014
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-10-29 17:42:19 +0100

    Add properties, constructor, and methods for MapElement class

--- Models/MapElement.cs
+++ Models/MapElement.cs
@@ -8,5 +8,21 @@ namespace Tank_Tactics.Models
 {
     public abstract class MapElement
     {
+        public string Image { get; set; }
+        public double PositionTop { get; }
+        public double PositionLeft { get; }
+        public double Width { get; }
+        public double Height { get; }
+
+        protected MapElement(string image, double positionTop, double positionLeft, double width, double height)
+        {
+            Image = image;
+            PositionTop = positionTop;
+            PositionLeft = positionLeft;
+            Width = width;
+            Height = height;
+        }
+        public bool IsTouching(Tank tank) => false;
+        public abstract void HandleTouch(Tank tank);
     }
 }
