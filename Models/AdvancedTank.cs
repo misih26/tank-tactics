c794b5454399756e4f8a3b506912548b6ee45ae7
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-10-29 17:35:37 +0100

    Add properties and constructor for AdvancedTank class, inheriting from Tank

--- Models/AdvancedTank.cs
+++ Models/AdvancedTank.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Tank_Tactics.Models
 {
-    public class AdvancedTank
+    public class AdvancedTank : Tank
     {
+        public double SecondaryAttack { get; set; }
+        public double SecondaryAttackCooldown { get; set; }
+        public AdvancedTank(string name, double shield, double attack, double attackCooldown, double speed, double secondaryAttack, double secondaryAttackCooldown)
+            : base(name, shield, attack, attackCooldown, speed)
+        {
+            SecondaryAttack = secondaryAttack;
+            SecondaryAttackCooldown = secondaryAttackCooldown;
+        }
     }
 }
