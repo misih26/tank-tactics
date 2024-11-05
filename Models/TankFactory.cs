58b74f0f25cd5c0c83485f3f94528777f0436ff3
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-11-05 19:06:45 +0100

    Add TankFactory class with BuildTank method for creating basic and advanced tanks

---
+++ Models/TankFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank_Tactics.Models
+{
+    public class TankFactory
+    {
+        public Tank BuildTank(Tanks tankType)
+        {
+            if (tankType == Tanks.Tiger)
+            {
+                // Alap tank (Tank típus)
+                return new Tank("Tiger", 150, 50, 2, 10, 0, 0, Direction.UP);
+            }
+            else if (tankType == Tanks.Leopard)
+            {
+                // Alap tank (Tank típus)
+                return new Tank("Leopard", 130, 45, 1.5, 12, 0, 0, Direction.UP);
+            }
+            else if (tankType == Tanks.Panther)
+            {
+                // Fejlett tank (AdvancedTank típus)
+                return new AdvancedTank("Panther", 140, 55, 1.8, 11, 25, 3, 0, 0, Direction.UP);
+            }
+            else if (tankType == Tanks.Sheridan)
+            {
+                // Fejlett tank (AdvancedTank típus)
+                return new AdvancedTank("Sheridan", 120, 40, 1.2, 13, 30, 2.5, 0, 0, Direction.UP);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid tank type");
+            }
+        }
+    }
+}
