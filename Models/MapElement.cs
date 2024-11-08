924f15167f0e0ae42ebb3e684fbd943496dd6d43
Misih26 <homonnai001@stud.uni-obuda.hu>
2024-11-08 16:29:51 +0100

    IsTouching method implemented

--- Models/MapElement.cs
+++ Models/MapElement.cs
@@ -24,12 +24,37 @@ namespace Tank_Tactics.Models
         }
         public bool IsTouching(Tank tank)
         {
-            return false;
+            int[] tankTopLeft = { (int)tank.PositionLeft, (int)tank.PositionTop };
+            int[] tankTopRight = { (int)(tank.PositionLeft + tank.Width), (int)tank.PositionTop };
+            int[] tankBottomLeft = { (int)tank.PositionLeft, (int)(tank.PositionTop + tank.Height) };
+            int[] tankBottomRight = { (int)(tank.PositionLeft + tank.Width), (int)(tank.PositionTop + tank.Height) };
+
+            int[] elementTopLeft = { (int)PositionLeft, (int)PositionTop };
+            int[] elementRightBottom = { (int)(PositionLeft + Width), (int)(PositionTop + Height) };
+
+            return IsSelectedCornerTouched(tankTopLeft, elementTopLeft, elementRightBottom) ||
+                   IsSelectedCornerTouched(tankTopRight, elementTopLeft, elementRightBottom) ||
+                   IsSelectedCornerTouched(tankBottomLeft, elementTopLeft, elementRightBottom) ||
+                   IsSelectedCornerTouched(tankBottomRight, elementTopLeft, elementRightBottom);
         }
         public abstract void HandleTouch(Tank tank);
 
         private bool IsSelectedCornerTouched(int[] tankCorner, int[] elementLeftTopConer, int[] elementRightBottomCorner)
         {
+            int tankX = tankCorner[0];
+            int tankY = tankCorner[1];
+
+            int elementLeftX = elementLeftTopConer[0];
+            int elementTopY = elementLeftTopConer[1];
+
+            int elementRightX = elementRightBottomCorner[0];
+            int elementBottomY = elementRightBottomCorner[1];
+
+            if (tankX >= elementLeftX && tankX <= elementRightX &&
+                tankY >= elementTopY && tankY <= elementBottomY)
+            {
+                return true;
+            }
             return false;
         }
     }
